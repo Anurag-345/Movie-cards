d7cf177cbad39eea79efb1ff30fef24d044e95b4
Anurag Nayajk <anuragnayak345@gmail.com>
2022-11-13 16:02:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Img from "./Images/dune.jpg";
+import Img4 from "./Images/thor.jpg";
+import Img2 from "./Images/endgame.jpg";
+import Cards from "./Components/Cards";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards movie="Dune" yearAndAuthor="2022, Amazon prime" images={Img} />
+      <Cards
+        movie="Avengers Endgame"
+        yearAndAuthor="2002, Marvel studios"
+        images={Img2}
+      />
+      <Cards movie="Thor Ragnarok" yearAndAuthor="2016, Marvel Studios" images={Img4} />
     </div>
   );
 }
